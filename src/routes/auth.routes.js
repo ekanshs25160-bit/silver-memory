1f1e9d82a0110c747863619584484fc5a774aefb
Ekansh Shrotriya <ekanshshrotriya@gmail.com>
2026-09-18 18:35:06 +0530

    feat: add endpoints for getting current user and changing password

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 // import { registerUser } from "../controllers/auth.controller.js";
-import { loginUser, logoutUser, registerUser } from "../controllers/auth.controller.js";
+import { getCurrentUser, loginUser, logoutUser, registerUser,changeCurrentPassword } from "../controllers/auth.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router= Router()
@@ -8,5 +8,7 @@ const router= Router()
 router.route('/register').post(registerUser)
 router.route('/login').post(loginUser)
 router.route('/logout').post(verifyJWT, logoutUser)
+router.route('/current-user').get(verifyJWT,getCurrentUser)
+router.route('/change-password').post(verifyJWT,changeCurrentPassword)
 
 export default router
